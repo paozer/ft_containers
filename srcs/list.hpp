f56c5733e21fe4a942ac6d83ea10762f2d471610
paozer <p.ramellaratin@gmail.com>
2020-08-01 17:27:55 +0200

    initial commit

---
+++ srcs/list.hpp
@@ -0,0 +1,123 @@
+#pragma once
+#ifndef LIST_HPP
+# define LIST_HPP
+
+# include <iostream>
+
+namespace ft {
+
+template < class T >
+struct node
+{
+    T _content;
+    node * _next;
+    node * _prev;
+};
+
+template < class T, class Alloc = std::allocator<T> >
+class list
+{
+    public:
+        typedef T value_type;
+        typedef Alloc allocator_type;
+        typedef typename allocator_type::reference reference;
+        typedef typename allocator_type::const_reference const_reference;
+        typedef typename allocator_type::pointer pointer;
+        typedef typename allocator_type::const_pointer const_pointer;
+        // typedef iterator
+        // typedef const_iterator
+        // typedef reverse_iterator
+        // typedef const_reverse_iterator
+        // typedef difference_type
+        typedef size_t size_type;
+
+        /* Constructors */
+        explicit list (const allocator_type& alloc = allocator_type());
+        explicit list (size_type n, const value_type& val = value_type(),
+                const allocator_type& alloc = allocator_type());
+        template <class InputIterator>
+        list (InputIterator first, InputIterator last,
+                const allocator_type& alloc = allocator_type());
+        list(const list& x);
+
+        /* Operators */
+        list &operator=(const list&);
+
+        /* Destructor */
+        ~list();
+
+        /* Iterators */
+
+        /* Capacity */
+        bool empty() const;
+        size_type size() const;
+        size_type max_size() const;
+
+        /* Element Access */
+        reference front();
+        const_reference front() const;
+        reference back();
+        const_reference back() const;
+
+        /* Modifiers */
+        template <class InputIterator>
+        void assign (InputIterator first, InputIterator last);
+        void assign (size_type n, const value_type& val);
+        void push_front (const value_type& val);
+        void pop_front();
+        void push_back (const value_type& val);
+        void pop_back();
+        //iterator insert (iterator position, const value_type& val);
+        //void insert (iterator position, size_type n, const value_type& val);
+        //template <class InputIterator>
+        //void insert (iterator position, InputIterator first, InputIterator last);
+        //iterator erase (iterator position);
+        //iterator erase (iterator first, iterator last);
+        void swap (list& x);
+        void resize (size_type n, value_type val = value_type());
+        void clear();
+
+        /* Operations */
+        //void splice (iterator position, list& x);
+        //void splice (iterator position, list& x, iterator i);
+        //void splice (iterator position, list& x, iterator first, iterator last);
+        void remove (const value_type& val);
+        template <class Predicate>
+        void remove_if (Predicate pred);
+        void unique();
+        template <class BinaryPredicate>
+        void unique (BinaryPredicate binary_pred);
+        void merge (list& x);
+        template <class Compare>
+        void merge (list& x, Compare comp);
+        void sort();
+        template <class Compare>
+        void sort (Compare comp);
+        void reverse();
+
+
+
+    private:
+        node<value_type> * _head;
+        size_type _size;
+};
+
+template <class T, class Alloc>
+bool operator== (const list<T,Alloc>& lhs, const list<T,Alloc>& rhs);
+template <class T, class Alloc>
+bool operator!= (const list<T,Alloc>& lhs, const list<T,Alloc>& rhs);
+template <class T, class Alloc>
+bool operator<  (const list<T,Alloc>& lhs, const list<T,Alloc>& rhs);
+template <class T, class Alloc>
+bool operator<= (const list<T,Alloc>& lhs, const list<T,Alloc>& rhs);
+template <class T, class Alloc>
+bool operator>  (const list<T,Alloc>& lhs, const list<T,Alloc>& rhs);
+template <class T, class Alloc>
+bool operator>= (const list<T,Alloc>& lhs, const list<T,Alloc>& rhs);
+
+template <class T, class Alloc>
+void swap (list<T,Alloc>& x, list<T,Alloc>& y);
+
+}
+
+#endif
