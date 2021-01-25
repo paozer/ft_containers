5fe62e1036fa2c71dcf36f64a5b5b3c85b1c9d2e
paozer <33629278+paozer@users.noreply.github.com>
2021-01-25 16:04:45 +0100

    list: added a few basic methods

--- srcs/list.hpp
+++ srcs/list.hpp
@@ -1,18 +1,24 @@
 #pragma once
-#ifndef LIST_HPP
-# define LIST_HPP
 
 # include <iostream>
 
 namespace ft {
 
-template < class T >
-struct node
-{
-    T _content;
-    node * _next;
-    node * _prev;
-};
+template < class T > struct Node;
+template < class T > class list_iterator;
+
+// use template parameter bool is_const ?
+//template < class value_type >
+//class list_iterator
+//{
+//    public:
+//        typedef std::bidirectional_iterator_tag iterator_category;
+
+//        list_iterator ( ft::list<value_type> & list )
+//            : _list(list) {};
+//    private:
+//        ft::list<value_type> & _list;
+//};
 
 template < class T, class Alloc = std::allocator<T> >
 class list
@@ -24,49 +30,89 @@ class list
         typedef typename allocator_type::const_reference const_reference;
         typedef typename allocator_type::pointer pointer;
         typedef typename allocator_type::const_pointer const_pointer;
-        // typedef iterator
-        // typedef const_iterator
-        // typedef reverse_iterator
-        // typedef const_reverse_iterator
-        // typedef difference_type
+        //typedef list_iterator<value_type> iterator;
+        //typedef list_iterator<const value_type> const_iterator;
+        //typedef reverse_iterator
+        //typedef const_reverse_iterator
+        //typedef difference_type
         typedef size_t size_type;
 
         /* Constructors */
-        explicit list (const allocator_type& alloc = allocator_type());
-        explicit list (size_type n, const value_type& val = value_type(),
-                const allocator_type& alloc = allocator_type());
-        template <class InputIterator>
+        explicit list (const allocator_type& alloc = allocator_type())
+            : _head(NULL), _size(0) {}
+
+        explicit list (size_t n, const value_type& val = value_type(),
+                const allocator_type& alloc = allocator_type())
+            : _head(NULL), _size(n)
+        {
+            for (size_type i = 0; i < _size; ++i)
+                push_front(val);
+        }
+
+        //substitution failure with second constructor
+        // use std::is_integral ?
+        // check sfinae cpp reference page
+        // https://stackoverflow.com/questions/45889587/c-sfinae-to-differentiate-fill-and-range-constructors
+        template <class InputIterator, typename std::iterator_traits<InputIterator>::value_type>
         list (InputIterator first, InputIterator last,
-                const allocator_type& alloc = allocator_type());
-        list(const list& x);
+                const allocator_type& alloc = allocator_type()) {}
+
+        list (const list& x);
 
         /* Operators */
         list &operator=(const list&);
 
         /* Destructor */
-        ~list();
+        ~list() {}
 
         /* Iterators */
+        //iterator begin();
+        //const_iterator begin() const;
+        //iterator end();
+        //const_iterator end() const;
+        //reverse_iterator rbegin();
+        //const_reverse_iterator rbegin() const;
+        //reverse_iterator rend();
+        //const_reverse_iterator rend() const;
 
         /* Capacity */
-        bool empty() const;
-        size_type size() const;
-        size_type max_size() const;
+        bool empty() const { return (_size == 0); }
+        size_type size() const { return (_size); }
+        size_type max_size() const { return (std::numeric_limits<size_type>::max()); }
 
         /* Element Access */
-        reference front();
-        const_reference front() const;
-        reference back();
-        const_reference back() const;
+        reference front ();
+        const_reference front () const;
+        reference back ();
+        const_reference back () const;
 
         /* Modifiers */
         template <class InputIterator>
         void assign (InputIterator first, InputIterator last);
         void assign (size_type n, const value_type& val);
-        void push_front (const value_type& val);
-        void pop_front();
+        void push_front (const value_type& val)
+        {
+            Node<value_type> * node = new Node<value_type>(val);
+
+            if (!_head)
+                _head = node;
+            else {
+                node->next = _head;
+                _head->prev = node;
+                _head = node;
+            }
+        }
+        void pop_front ()
+        {
+            if (!_head)
+                return ;
+            Node<value_type> * tmp = _head;
+            _head = _head->next;
+            --_size;
+            delete tmp;
+        }
         void push_back (const value_type& val);
-        void pop_back();
+        void pop_back ();
         //iterator insert (iterator position, const value_type& val);
         //void insert (iterator position, size_type n, const value_type& val);
         //template <class InputIterator>
@@ -75,7 +121,7 @@ class list
         //iterator erase (iterator first, iterator last);
         void swap (list& x);
         void resize (size_type n, value_type val = value_type());
-        void clear();
+        void clear ();
 
         /* Operations */
         //void splice (iterator position, list& x);
@@ -95,12 +141,11 @@ class list
         void sort (Compare comp);
         void reverse();
 
-
-
     private:
-        node<value_type> * _head;
+        Node<value_type> * _head;
         size_type _size;
-};
+
+}; // class list
 
 template <class T, class Alloc>
 bool operator== (const list<T,Alloc>& lhs, const list<T,Alloc>& rhs);
@@ -118,6 +163,14 @@ bool operator>= (const list<T,Alloc>& lhs, const list<T,Alloc>& rhs);
 template <class T, class Alloc>
 void swap (list<T,Alloc>& x, list<T,Alloc>& y);
 
-}
+template < class T >
+struct Node
+{
+    T content;
+    Node * next;
+    Node * prev;
 
-#endif
+    Node(T contentt) : content(contentt), next(NULL), prev(NULL) {};
+};
+
+} // namespace ft
