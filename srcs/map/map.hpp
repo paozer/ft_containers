868e20a30203b94df9293b2a9915bd9e94148d74
paozer <33629278+paozer@users.noreply.github.com>
2021-02-13 16:33:04 +0100

    map: initial commit

---
+++ srcs/map/map.hpp
@@ -0,0 +1,124 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <limits>
+#include <type_traits>
+#include "map_node.hpp"
+#include "map_iterator.hpp"
+
+namespace ft {
+
+template < class Key,
+           class T,
+           class Compare = std::less<Key>,
+           class Alloc = std::allocator<std::pair<const Key, T> >
+           >
+class map
+{
+
+    public:
+        typedef Key key_type;
+        typedef T mapped_type;
+        typedef std::pair<const key_type, mapped_type> value_type;
+        typedef Compare key_compare;
+        typedef Alloc allocator_type;
+        typedef typename allocator_type::reference reference;
+        typedef typename allocator_type::const_reference const_reference;
+        typedef typename allocator_type::pointer pointer;
+        typedef typename allocator_type::const_pointer const_pointer;
+
+        typedef map_iterator<value_type, false> iterator;
+        typedef map_iterator<value_type, true> const_iterator;
+        typedef reverse_map_iterator<value_type, false> reverse_iterator;
+        typedef reverse_map_iterator<value_type, true> const_reverse_iterator;
+        typedef std::ptrdiff_t difference_type;
+        typedef size_t size_type;
+
+    private:
+        typedef map_node<value_type> node;
+        typedef node * node_pointer;
+        typedef typename Alloc::template rebind<node>::other node_allocator;
+
+    public:
+        /* Constructors */
+        explicit map (const allocator_type& alloc = allocator_type())
+            : _size(0), _root(NULL), _key_comp(key_compare())
+        {
+        }
+        explicit map (size_t n, const value_type & val = value_type(), const allocator_type& alloc = allocator_type());
+        template <class InputIterator>
+        map (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type(),
+              typename ft::enable_if< !std::is_integral<InputIterator>::value , void >::type* = 0);
+        map (const map & x);
+
+        /* Destructor */
+        ~map();
+
+        /* Operators */
+        map &operator=(const map & x);
+        mapped_type& operator[] (const key_type& k);
+        friend bool operator== (const map<T, Alloc>& lhs, const map<T, Alloc>& rhs);
+        friend bool operator< (const map<T, Alloc>& lhs, const map<T, Alloc>& rhs);
+        friend bool operator!= (const map<T, Alloc>& lhs, const map<T, Alloc>& rhs) { return !(lhs == rhs); }
+        friend bool operator<= (const map<T, Alloc>& lhs, const map<T, Alloc>& rhs) { return !(rhs < lhs); }
+        friend bool operator> (const map<T, Alloc>& lhs, const map<T, Alloc>& rhs) { return rhs < lhs; }
+        friend bool operator>= (const map<T, Alloc>& lhs, const map<T, Alloc>& rhs) { return !(lhs < rhs); }
+
+        /* Iterators */
+        //iterator begin() { return iterator(_head->next); }
+        //const_iterator begin() const { return const_iterator(_head->next); }
+        //iterator end() { return iterator(_tail); }
+        //const_iterator end() const { return const_iterator(_tail); }
+
+        //reverse_iterator rbegin() { return reverse_iterator(_tail->prev); }
+        //const_reverse_iterator rbegin() const { return const_reverse_iterator(_tail->prev); }
+        //reverse_iterator rend() { return reverse_iterator(_head); }
+        //const_reverse_iterator rend() const { return const_reverse_iterator(_head); }
+
+        /* Capacity */
+        bool empty() const { return _size == 0; }
+        size_type size() const { return _size; }
+        size_type max_size() const { return _alloc.max_size(); }
+
+        /* Modifiers */
+        std::pair<iterator, bool> insert (const value_type& val);
+        iterator insert (iterator position, const value_type& val);
+        template <class InputIterator>
+        void insert (InputIterator first, InputIterator last,
+              typename ft::enable_if< !std::is_integral<InputIterator>::value , void >::type* = 0);
+
+        void erase (iterator position);
+        size_type erase (const key_type& k);
+        void erase (iterator first, iterator last);
+        void clear (void);
+        void swap (map& x); // usage of std::swap in <algorithm> forbidden ?
+
+        /* Operations */
+        size_type count (const key_type& k) const;
+        iterator find (const key_type& k);
+        const_iterator find (const key_type& k) const;
+
+        std::pair<iterator, iterator> equal_range (const key_type& k);
+        std::pair<const_iterator, const_iterator> equal_range (const key_type& k) const;
+        iterator lower_bound (const key_type& k);
+        const_iterator lower_bound (const key_type& k) const;
+        iterator upper_bound (const key_type& k);
+        const_iterator upper_bound (const key_type& k) const;
+
+        /* Observers */
+        key_compare key_comp() const { return _key_comp; }
+        //value_compare value_comp() const;
+
+    private:
+        size_type _size;
+        node_pointer _root;
+        node_allocator _alloc;
+        key_compare _key_comp;
+
+}; // class map
+
+template <class T, class Alloc>
+void swap (map<T,Alloc>& x, map<T,Alloc>& y);
+
+} // namespace ft
---
+++ srcs/map/map_iterator.hpp
@@ -0,0 +1,147 @@
+#pragma once
+
+#include "map_node.hpp"
+#include "../utils.hpp"
+#include <iterator>
+
+namespace ft {
+
+template <class T, bool is_const>
+class map_iterator
+{
+    public:
+        typedef T value_type;
+        typedef std::ptrdiff_t difference_type;
+        typedef typename choose<is_const, const T *, T *>::type pointer;
+        typedef typename choose<is_const, const T &, T &>::type reference;
+        typedef std::bidirectional_iterator_tag iterator_category;
+
+    private:
+        typedef map_iterator<value_type, is_const> self_type;
+        typedef typename choose<is_const, const map_node<T> *, map_node<T> *>::type node_pointer;
+
+    public:
+
+        // use typedef node_pointer
+        map_iterator (map_node<value_type> * node = NULL) : _node(node) {}
+
+        map_iterator (const map_iterator<value_type, false>& other) : _node(other.get_node()) {}
+
+        map_iterator &operator=(const self_type & other)
+        {
+            if (*this != other)
+                _node = other._node;
+            return *this;
+        }
+
+        bool operator==(const self_type & other) { return _node == other._node; }
+        bool operator!=(const self_type & other) { return _node != other._node; }
+
+        pointer operator->(void) { return &(_node->content); }
+        reference operator*(void) { return _node->content; }
+
+        self_type & operator++(void)
+        {
+            _node = _node->next;
+            return *this;
+        }
+
+        self_type operator++(int)
+        {
+            self_type tmp = *this;
+            _node = _node->next;
+            return tmp;
+        }
+
+        self_type & operator--(void)
+        {
+            _node = _node->prev;
+            return *this;
+        }
+
+        self_type operator--(int)
+        {
+            self_type tmp = *this;
+            _node = _node->prev;
+            return tmp;
+        }
+
+        node_pointer get_prev(void) const { return _node->prev; }
+        node_pointer get_next(void) const { return _node->next; }
+        node_pointer get_node(void) const { return _node; }
+
+    private:
+        node_pointer _node;
+
+}; // class map_iterator
+
+template <class T, bool is_const>
+class reverse_map_iterator
+{
+    public:
+        typedef T value_type;
+        typedef std::ptrdiff_t difference_type;
+        typedef typename choose<is_const, const T *, T *>::type pointer;
+        typedef typename choose<is_const, const T &, T &>::type reference;
+        typedef std::bidirectional_iterator_tag iterator_category;
+
+    private:
+        typedef reverse_map_iterator<value_type, is_const> self_type;
+        typedef typename choose<is_const, const map_node<T> *, map_node<T> *>::type node_pointer;
+
+    public:
+
+        // use typedef node_pointer
+        reverse_map_iterator (map_node<value_type> * node = NULL) : _node(node) {}
+
+        reverse_map_iterator (const reverse_map_iterator<value_type, false>& other) : _node(other.get_node()) {}
+
+        reverse_map_iterator &operator=(const self_type & other)
+        {
+            if (*this != other)
+                _node = other._node;
+            return *this;
+        }
+
+        bool operator==(const self_type & other) { return _node == other._node; }
+        bool operator!=(const self_type & other) { return _node != other._node; }
+
+        pointer operator->(void) { return &(_node->content); }
+        reference operator*(void) { return _node->content; }
+
+        self_type & operator++(void)
+        {
+            _node = _node->prev;
+            return *this;
+        }
+
+        self_type operator++(int)
+        {
+            self_type tmp = *this;
+            _node = _node->prev;
+            return tmp;
+        }
+
+        self_type & operator--(void)
+        {
+            _node = _node->next;
+            return *this;
+        }
+
+        self_type operator--(int)
+        {
+            self_type tmp = *this;
+            _node = _node->next;
+            return tmp;
+        }
+
+        node_pointer get_prev(void) const { return _node->prev; }
+        node_pointer get_next(void) const { return _node->next; }
+        node_pointer get_node(void) const { return _node; }
+
+    private:
+        node_pointer _node;
+
+}; // class reverse_map_iterator
+
+} // namespace ft
