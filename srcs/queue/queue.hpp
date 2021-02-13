12fddcfef9ce2c1af04a05db9eb0003ce7f0c15c
paozer <33629278+paozer@users.noreply.github.com>
2021-02-13 17:54:40 +0100

    queue: initial commit

---
+++ srcs/queue/queue.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "../list/list.hpp"
+
+namespace ft {
+
+template < class T, class Container = list<T> >
+class queue
+{
+
+    public:
+        typedef T value_type;
+        typedef Container container_type;
+        typedef size_t size_type;
+
+    public:
+        explicit queue (const container_type& cntr = container_type())
+            : _cntr(cntr)
+        {
+        }
+
+        ~queue()
+        {
+        }
+
+        bool empty() const { return _cntr.empty(); }
+        size_type size() const { return _cntr.size(); }
+
+        value_type& front() { return _cntr.front(); }
+        const value_type& front() const { return _cntr.front(); }
+        value_type& back() { return _cntr.back(); }
+        const value_type& back() const { return _cntr.back(); }
+
+        void push (const value_type& val) { _cntr.push_back(val); }
+        void pop(void) { _cntr.pop_back(); }
+
+        bool operator== (const queue<T, Container>& rhs) { return _cntr == rhs._cntr; }
+        bool operator< (const queue<T, Container>& rhs) { return _cntr < rhs._cntr; }
+        bool operator!= (const queue<T, Container>& rhs) { return _cntr != rhs._cntr; }
+        bool operator<= (const queue<T, Container>& rhs) { return _cntr <= rhs._cntr; }
+        bool operator> (const queue<T, Container>& rhs) { return _cntr > rhs._cntr; }
+        bool operator>= (const queue<T, Container>& rhs) { return _cntr >= rhs._cntr; }
+
+    private:
+        container_type _cntr;
+
+}; // class queue
+
+} // namespace ft
